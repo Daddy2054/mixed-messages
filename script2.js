0c342ff03b701eb0cbfa20651724ab18af958ff5
Jean <acalto2@gmail.com>
2022-06-27 21:40:25 +0200

    ok

---
+++ script2.js
@@ -0,0 +1,14 @@
+const quotes = [
+  ["Winning isn't everything, but wanting to win is. Vince Lombardi"],
+  ["Failure is simply the opportunity to begin again, this time more intelligently. Henry Ford"],
+  ["Judge your success by what you had to give up in order to get it. Dalai Lama"],
+  ["Wheresoever you go, go with all your heart. Confucius"],
+  ["A comfort zone is a beautiful place, but nothing ever grows there. Unknown"],
+  ["It is in your moments of decision that your destiny is shaped. Tony Robbins"],
+  ["He is richest who is content with the least, for content is the wealth of nature. Socrates"],
+  ["The world makes way for the man who knows where he is going. Ralph Waldo Emerson"],
+  ["Wealth is the product of man's capacity to think. Ayn Rand"],
+  ["Do not follow the ideas of others, but learn to listen to the voice within yourself. Dogen"]
+];
+let num = Math.floor(Math.random() * 11);
+console.log(quotes[num]);
